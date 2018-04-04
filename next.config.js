be1dfb3e5dcdbd05563dbce9d00f9d04c32e0a46
rhiokim <rhio.kim@gmail.com>
2018-04-05 00:46:23 +0900

    feat(server): apply next-bundle-analyze

--- next.config.js
+++ next.config.js
@@ -1,22 +1,37 @@
 const withSass = require('@zeit/next-sass')
 const withOffline = require('next-offline')
+const withBundleAnalyzer = require('@zeit/next-bundle-analyzer')
 
 module.exports = withSass(
-  withOffline({
-    webpack: config => {
-      // Fixes npm packages that depend on `fs` module
-      config.node = {
-        fs: 'empty'
-      }
+  withBundleAnalyzer({
+    ...withOffline({
+      webpack: config => {
+        // Fixes npm packages that depend on `fs` module
+        config.node = {
+          fs: 'empty'
+        }
 
-      return config
-    },
-    exportPathMap: () => ({
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-      '/elements': { page: '/elements' },
-      '/example': { page: '/example' },
-      '/posts': { page: '/posts' }
-    })
+        return config
+      },
+      exportPathMap: () => ({
+        '/': { page: '/' },
+        '/about': { page: '/about' },
+        '/elements': { page: '/elements' },
+        '/example': { page: '/example' },
+        '/posts': { page: '/posts' }
+      })
+    }),
+    analyzeServer: ['server', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    analyzeBrowser: ['browser', 'both'].includes(process.env.BUNDLE_ANALYZE),
+    bundleAnalyzerConfig: {
+      server: {
+        analyzerMode: 'static',
+        reportFilename: '../../.bundles/server.html'
+      },
+      browser: {
+        analyzerMode: 'static',
+        reportFilename: '../.bundles/client.html'
+      }
+    }
   })
 )
